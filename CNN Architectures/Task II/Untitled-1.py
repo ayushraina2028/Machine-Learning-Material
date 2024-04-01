5d1739115dfbd7da5fad82db3983fa0b0d74a9d9
ayushraina <ayushraina@iisc.ac.in>
2024-04-01 19:44:45 +0530

    Custom CNN Implementation

--- CNN Architectures/Task II/Untitled-1.py
+++ CNN Architectures/Task II/Untitled-1.py
@@ -2,22 +2,33 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
-import numpy as np
+
 # %%
 import pyarrow.parquet as pq
 import pandas as pd
-pf = pd.read_parquet('data.parquet',dtype_backend = 'pyarrow')
+import numpy as np
+df = pd.read_parquet('run1.parquet',dtype_backend = 'pyarrow')[['X_jets','y']]
 
 # %%
-df = pf[['X_jets','y']][:50]
+# Set device to cuda
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
+# %%
+df = df.iloc[:500]
 
 # %%
 # Convert to numpy arrays
 X = np.stack(df['X_jets'].values)
 y = df['y'].values
 
+
 # %%
 print(X.shape, y.shape)
+
+# %%
+df = None
 
 # %%
 # Separate into training, validation, and test data
@@ -70,7 +81,7 @@ std = np.std(X_train, axis=(0,1,2,3))
 # %%
 X_train = (X_train - mean) / (std + 1e-7)
 X_valid = (X_valid - mean) / (std + 1e-7)
-X_test = (X_test - mean) / (std + 1e-7)
+#X_test = (X_test - mean) / (std + 1e-7)
 
 # %%
 # Import necessary libraries
@@ -91,12 +102,12 @@ model = Sequential()
 
 
 # %%
-from skimage.transform import resize
+# from skimage.transform import resize
 
-# Resize all images to 32,32 in X_train
-# X_train = np.array([resize(image, (32,32)) for image in X_train])
-# X_valid = np.array([resize(image, (32,32)) for image in X_valid])
-# X_test = np.array([resize(image, (32,32)) for image in X_test])
+# # Resize all images to 32,32 in X_train
+# X_train = np.array([resize(image, (128,128)) for image in X_train])
+# X_valid = np.array([resize(image, (128,128)) for image in X_valid])
+# X_test = np.array([resize(image, (128,128)) for image in X_test])
 
 print(X_train.shape, X_valid.shape, X_test.shape)
 
@@ -163,11 +174,11 @@ model.summary()
 
 
 # %%
-batch_size = 16
-epochs = 10
+batch_size = 128
+epochs = 40
 
 #USe model checkpoint to save only the best model
-checkpointer = ModelCheckpoint(filepath='model.weights.best.keras', verbose=1, save_best_only=True)
+checkpointer = ModelCheckpoint(filepath='model.weights.best.keras', verbose=1, save_best_only=True, monitor='val_accuracy', mode='max')
 
 #adam optimizer
 opt = keras.optimizers.Adam(learning_rate=0.001)
@@ -175,11 +186,20 @@ opt = keras.optimizers.Adam(learning_rate=0.001)
 model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 # %%
-# # Make data as tensor
-# import tensorflow as tf
-# X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
-# X_valid = tf.convert_to_tensor(X_valid, dtype=tf.float32)
-# X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
+import torch    
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
+# %%
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import tensorflow as tf
+
+
+
+import tensorflow as tf
+tf.config.list_physical_devices()
+
 
 # %%
 history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_valid, y_valid), callbacks=[checkpointer], verbose=2, shuffle=True)
@@ -187,6 +207,28 @@ history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, vali
 # %%
 scores = model.evaluate(X_test, y_test, batch_size=batch_size, verbose=2)
 print("\nTest result: %.3f loss: %.3f" % (scores[1]*100,scores[0]))
+
+# %%
+# plot learning curves
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['accuracy'], label='train-accuracy')
+plt.plot(history.history['val_accuracy'], label='val-accuracy')
+plt.legend()
+plt.show()
+
+#save this graph
+plt.savefig('accuracy.png')
+
+# %%
+# Plot traning Loss and validation loss
+plt.plot(history.history['loss'], label='train-loss')
+plt.plot(history.history['val_loss'], label='val-loss')
+plt.legend()
+plt.show()
+
+# save this graph
+plt.savefig('loss.png')
 
 # %%
 #Save the history in a file named accuracies.txt (loss, everthing)
@@ -212,10 +254,18 @@ with open('accuracies.txt', 'w') as f:
     f.write(str(history.history['val_accuracy']))
     f.write('\n')
     
-    # save test result
-    f.write('\nTest result: ')
-    f.write(str(scores[1]*100))
-    f.write('\n')
     
-    
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
 
